744b034ba1fa485f430ef14559a4a3039a343a38
jllerenac <josellerenacarpio@gmail.com>
2019-08-11 22:01:32 -0500

    After implementing slim

---
+++ app/assets/javascripts/ajax-req.js
@@ -0,0 +1,28 @@
+function a_req()
+{
+    var b = $('#b').val();
+    var t = $('#t').val();
+    if (b.length === 0 || t.length === 0)
+    {
+        alert('No empty values');
+        return false;  
+    }
+    if (t.length < 5)
+    {
+        alert('Title has to be at least 5 characters long');
+        return false;  
+    }
+    $.ajax(
+        {
+        type : 'POST',
+        data : 
+        {
+            t: t,b: b},
+        url : ('questions'),
+        success :   function(result)
+        {
+            Alert('Datos ingresados','AVISO');
+        }
+        });
+    return false;
+}
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, alert: 'you must sign in first!'
+  respond_to :html, :js
     def index
         @questions = Question.all
     end
@@ -8,24 +10,44 @@ class QuestionsController < ApplicationController
     def new
         @question = Question.new
     end
+    def edit
+      @question = Question.find(params[:id])
+    end
     def create
-        @question = Question.new(question_params)
-       
-        if @question.save
-          redirect_to @question
-        else
-          render 'new'
-        end
+ #       @question = Question.new(question_params)
+      @question = Question.create(question_params)
+      respond_to do |f|
+        f.js { render 'new' }
       end
-      def check_for_back
-        if params[:commit] == "Back"
-          redirect_to questions_path
-        end
+  #      if @question.save
+  #        redirect_to questions_path
+  #      else
+  #        redirect_to questions_path
+  #      end
+    end
+    def update
+      @question = Question.find(params[:id])
+ 
+      if @question.update(question_params)
+        redirect_to questions_path
+      else
+        render 'edit'
       end
-      private
-        def question_params
-          params.require(:question).permit(:title, :text, :user_id)
-        end
+    end
+    def destroy
+      @question = Question.find(params[:id])
+      @question.destroy
+      redirect_to questions_path
+    end
+    def check_for_back
+      if params[:commit] == "Back"
+        redirect_to questions_path
+      end
+    end
+  private
+    def question_params
+      params.require(:question).permit(:title, :text, :user_id)
+    end
 
 
 end
